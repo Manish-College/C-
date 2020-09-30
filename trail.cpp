e8038f0eac5afb19300219b74ae027ee3544cc80
Manish-College <72164691+Manish-College@users.noreply.github.com>
2020-10-01 01:17:23 +0530

    Add files via upload

---
+++ trail.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[2];
+
+    cout<<arr[3]<<" "<<arr[-2]<<" ";
+    // int arr1[2] = {10, 20, 30, 40}; This will give error
+}
---
+++ trial2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int z=0; z<n; z++)
+    {
+        cout<<endl;
+        long int a;
+        cin>>a;
+        if(a<1 || a>2*100000)
+            break;
+        string arr[200];
+        for(int y=0; y<((8*a)-2); y++)
+        {
+            cin>>arr[y];
+        }
+        string a1[100];
+        int b=0;
+
+        for(int y=0; y<((4*a)-1); y++)
+        {
+            a1[y] = arr[b];
+            b=b+2;
+        }
+        
+        string a2[100];
+        b=1;
+        for(int y=0; y<((4*a)-1); y++)
+        {
+            a2[y] = arr[b];
+            b=b+2;
+        }
+        sort(a1, a1+((4*a)-1));
+        sort(a2, a2+((4*a)-1));
+        for(int i=0; i<((4*a)-1); i=i+2)
+        {
+            if(a1[i]!=a1[i+1])
+            {
+                cout<<a1[i]<<" ";
+                break;
+            }
+        }
+        for(int i=0; i<((4*a)-1); i=i+2)
+        {
+            if(a2[i]!=a2[i+1])
+            {
+                cout<<a2[i];
+                break;
+            }
+        }
+        cout<<endl;
+        
+        
+
+
+    }
+}
